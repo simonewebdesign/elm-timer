f03c530c892aefa0bf1a631bd244c2b313043262
Simone Vittori <hello@simonewebdesign.it>
2016-04-22 19:34:27 +0100

    Document code

--- Timer.elm
+++ Timer.elm
@@ -8,23 +8,60 @@ module Timer
   , tick, countdown
   ) where
 
+{-| This module provides a simple timer that you can easily integrate in your Elm app.
+It's even easier if you are following the [the Elm Architecture][arch] and using
+[start-app][].
+
+[arch]: https://github.com/evancz/elm-architecture-tutorial
+[start-app]: http://package.elm-lang.org/packages/evancz/start-app/latest/
+
+# Types
+@docs Model, Action
+
+# Functions
+@docs init, init', view, update
+
+# Signals
+@docs tick, countdown
+
+# Extras
+@docs addLeadingZero, clock
+-}
+
 import Signal exposing (map, foldp)
 import Time exposing (every, second)
 
 
+{-| Type alias that represents the internal value of a timer.
+It's basically just a simple counter.
+-}
 type alias Model = Int
 
 
+{-| Initialize a timer.
+-}
 init : Model
 init =
   0
 
 
+{-| Initialize a timer with a starting value, in seconds.
+
+    Timer.init' (round Time.hour)
+
+    -- or, if you like magic numbers
+    Timer.init' 3600
+-}
 init' : Model -> Model
 init' model =
   model
 
 
+{-| Provides a way to display the timer, in a format that
+looks like 01:30 (always with leading zeros).
+
+    Timer.view (Timer.init 30) -- will return "00:30"
+-}
 view : Model -> String
 view model =
   let
@@ -43,11 +80,22 @@ view model =
     minutes ++ ":" ++ seconds
 
 
+{-| A type that you can use to update the timer.
+A `Tick` moves forward, a `Tock` goes backwards.
+-}
 type Action
   = Tick
   | Tock
 
 
+{-| Updates the model.
+This is how you can forward actions to a timer using the Elm Architecture:
+
+    TimerAction subAction ->
+      ( { model | timer = Timer.update subAction model.timer }
+      , Effects.none
+      )
+-}
 update : Action -> Model -> Model
 update action model =
   case action of
@@ -61,6 +109,12 @@ update action model =
         model
 
 
+{-| Adds a leading zero to a number *only if* it's a single digit.
+
+    addLeadingZero 0 == "0"
+    addLeadingZero 1 == "01"
+    addLeadingZero 12 == "12"
+-}
 addLeadingZero : number -> String
 addLeadingZero num =
   if num < 10 then
@@ -69,6 +123,8 @@ addLeadingZero num =
     toString num
 
 
+{-| A counter that increments every second.
+-}
 clock : Signal Model
 clock =
   foldp (+) 0 (every second)
@@ -82,11 +138,18 @@ clock =
   --       |> map round
 
 
+{-| A `Signal` that tells a `Timer` to step forward.
+Use this if you want a timer that goes to the future.
+-}
 tick : Signal Action
 tick =
   map (always Tick) clock
 
 
+{-| A `Signal` that tells a `Timer` to step backwards.
+Use this if you want a timer that goes to the past.
+This will have no effect if the time is already expired (00:00).
+-}
 countdown : Signal Action
 countdown =
   map (always Tock) clock
